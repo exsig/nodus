4039f3ec9a2dc6ac9301f07dc683a1401d59b1b3
Joseph Wecker <joseph.wecker@exsig.com>
2014-05-07 14:03:07 -0700

    Simplify lag node and fix old module name in fdiff

--- lib/nodus.rb
+++ lib/nodus.rb
@@ -2,22 +2,15 @@ require 'nodus/version'
 module Nodus
   module Node
     class Lag
-      def initialize(size)
-        @size = size
-        @buff = @size > 1 ? Array.new(@size, nil) : nil
-      end
-      def next(x)
-        if @size > 1 then res = @buff.shift; @buff << x
-        else              res = @buff;       @buff  = x end
-        res
-      end
+      def initialize(size) @buff = Array.new(size, nil) end
+      def next(x) (@buff << x).shift end
     end
 
     class FDiff
       def initialize(lag=1, cap=4)
         @lag = Lag.new(lag)
         @cap = cap
-        @next_order = BackwardFiniteDiff.new(1, @cap - 1) if @cap > 1
+        @next_order = FDiff.new(1, @cap - 1) if @cap > 1
       end
 
       def next(x)
