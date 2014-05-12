69b3da771bc929f67802ac1eb33aefd8a8ff8016
Joseph Wecker <joseph.wecker@exsig.com>
2014-05-12 12:09:55 -0700

    Rework tests a bit to use minitest/spec and some random data generation

--- Gemfile
+++ Gemfile
@@ -9,12 +9,16 @@ gem 'sequel_postgresql_triggers'
 # Dependencies for gem development
 # (Includes everything needed to run rake, tests, features, etc.)
 group :development do
-  gem 'brice'
-  gem 'ansi'
-  gem 'minitest', '>= 5.3'
-  gem 'minitest-reporters', '>= 1.0.1'
-  gem 'rdoc'
   gem 'bundler'
   gem 'jeweler'
+  gem 'brice'
+  gem 'ansi'
+  gem 'rdoc'
+
+  # Testing
+  gem 'faker'
+  gem 'randexp'
+  gem 'minitest', '>= 5.3'
+  gem 'minitest-reporters', '>= 1.0.1'
   gem 'simplecov', '~> 0.7.1', require: false
 end
--- test/test_nodus.rb
+++ test/test_nodus.rb
@@ -1,36 +1,36 @@
 require 'helper'
 
-class NodesTest < MiniTest::Unit::TestCase
-  def test_node
-    # 
-
-
-  end
-
-  def test_lag_node
-    lag = Nodus::Node::Lag.new(1)
-    assert_equal  nil, lag.next(1)
-    assert_equal  1,   lag.next(2)
-    assert_equal  2,   lag.next(-1)
-    assert_equal -1,   lag.next(2)
-
-    lag = Nodus::Node::Lag.new(5)
-    5.times{|n| lag.next n}
-    assert_equal 0, lag.next(5)
-  end
-
-
-end
-
-
-class BufferedStreamTest < MiniTest::Unit::TestCase
-
-  class BSTest < Nodus::BufferedStream
-    
-  end
-
-  def test_temporary_buffered_stream
-    b = BSTest.new
-    #b << 
-  end
-end
+#class NodesTest < MiniTest::Unit::TestCase
+#  def test_node
+#    # 
+#
+#
+#  end
+#
+#  def test_lag_node
+#    lag = Nodus::Node::Lag.new(1)
+#    assert_equal  nil, lag.next(1)
+#    assert_equal  1,   lag.next(2)
+#    assert_equal  2,   lag.next(-1)
+#    assert_equal -1,   lag.next(2)
+#
+#    lag = Nodus::Node::Lag.new(5)
+#    5.times{|n| lag.next n}
+#    assert_equal 0, lag.next(5)
+#  end
+#
+#
+#end
+#
+#
+#class BufferedStreamTest < MiniTest::Unit::TestCase
+#
+#  class BSTest < Nodus::BufferedStream
+#    
+#  end
+#
+#  def test_temporary_buffered_stream
+#    b = BSTest.new
+#    #b << 
+#  end
+#end
