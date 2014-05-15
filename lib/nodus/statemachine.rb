9bff269265e794077890e9d69376db11cafa2c2c
Joseph Wecker <joseph.wecker@exsig.com>
2014-05-14 17:21:51 -0700

    Basic statemachine and the first several tests

--- lib/nodus/statemachine.rb
+++ lib/nodus/statemachine.rb
@@ -1,39 +1,49 @@
+# Different from the usual Ruby statemachine implementations where methods are more or less events and states and
+# transitions are declaratively defined. In this implementation you simply start_statemachine() and the initial state
+# method is invoked. It does its thing and possibly blocks or yields, and eventually returns a symbol for the next state
+# to transition to. In other words- the natural way state machines are implemented in more functional languages,
+# modified slightly because Ruby generally lacks tail-call elimination.
+#
+
 module Nodus
   module StateMachine
-    def self.included(s)
-      s.instance_methods(false).each do |m|
-        next unless m =~ /^state_/
-        # TODO: Allow dropping 'state_' in next_state return value
-        s.class_eval %Q{private :#{m.to_sym}}
-      end
-    end
-  end
+    def self.included(k) k.extend ClassMethods end
 
-  class TestMachine
-    def test(a)
-      puts 'hi'
-    end
-
-    # TODO: YOU ARE HERE. move this into statemachine and expand as appropriate
-    def run
-      @current_state = :state_initial
+    def start_statemachine(initial_state = nil)
+      @current_state = initial_state || @initial_state || :initial
       loop do
-        @current_state = self.send(@current_state)
-        raise ArgumentError, "Didn't return a valid state." unless self.respond_to?(@current_state.to_s, true)
+        @current_state = self.send("__#{@current_state}")
+        break if @current_state == :done
       end
     end
 
-    def state_initial
-      puts 'in the state'
-      :state_next
+    module ClassMethods
+      # Simply overrides the method(s) with an error if called directly, and sets up one (or possibly two- a second
+      # without 'state_' prepended) methods with leading underscores that the statemachine runtime loop calls as
+      # appropriate for original functionality.
+      #
+      # It's implemented here by prepending anonymous modules. This is so that everything can be set up within the class
+      # even if the module is included and state_method(...)'s are declared before any of the methods are actually
+      # defined. See for example: http://gshutler.com/2013/04/ruby-2-module-prepend/
+      #
+      def state(*method_names)
+        method_names.each do |mname|
+          anon_module = Module.new do
+            define_method mname do super end # Sets up method with original functionality even if it's not defined yet in the including class
+            alias_method "__#{mname}", mname
+            alias_method("__#{mname[6..-1]}", mname) if mname =~ /^state_/
+            define_method mname do
+              raise RuntimeError, "Sorry, this is a state method now. If you really, really need to call it like a normal function, send :__#{mname}"
+            end
+            private "__#{mname}"
+            private "__#{mname[6..-1]}" if mname =~ /^state_/
+          end
+          prepend anon_module
+        end
+      end
+      alias_method :states,        :state
+      alias_method :state_method,  :state
+      alias_method :state_methods, :state
     end
-
-
-    def state_next
-
-      :state_initial
-    end
-
-    include StateMachine
   end
 end
