05e00d7ff700a002065e2e569fa804cbaa432a77
Joseph Wecker <joseph.wecker@exsig.com>
2014-06-03 10:37:25 -0700

    Clean up oanda param processing some more

--- lib/nodus/fx/oanda.rb
+++ lib/nodus/fx/oanda.rb
@@ -4,48 +4,38 @@ module Nodus
   module FX
     class Oanda < Nodus::Node
       attr_reader :instruments, :account_id
-      def parameterize(source=:production, access_token=nil, instruments=nil)
-        @source       = source
-        @access_token = access_token
 
-        # TODO: raise error if no ENV['...'] and also none specified and one is needed (i.e., not  sandbox)
+      PARAMS = {production: {access_var:  'OANDA_ACCESS_TOKEN',
+                             api_root:    'https://api-fxtrade.oanda.com/',
+                             stream_root: 'https://stream-fxtrade.oanda.com/'},
+                practice:   {access_var:  'OANDA_ACCESS_TOKEN_PRACTICE',
+                             api_root:    'https://api-fxpractice.oanda.com/',
+                             stream_root: 'https://stream-fxpractice.oanda.com/'}}
 
-        case @source
-        when :production
-          @access_token ||= ENV['OANDA_ACCESS_TOKEN']
-          @api_root       = 'https://api-fxtrade.oanda.com/'
-          @stream_root    = 'https://stream-fxtrade.oanda.com/'
-        when :practice
-          @access_token ||= ENV['OANDA_ACCESS_TOKEN_PRACTICE']
-          @api_root       = 'https://api-fxpractice.oanda.com/'
-          @stream_root    = 'https://stream-fxpractice.oanda.com/'
-        when :sandbox
-          error NotImplementedError, "Sandbox Oanda access not implemented- should be easy if you want it."
-        else
-          error ArgumentError, "Oanda generator source must be one of [:production, :practice, :sandbox]"
-        end
+      def defaults() PARAMS[@source] || {} end
 
-        @api_conn = Excon.new(@api_root, headers: {'Authorization' => "Bearer #{@access_token}"}, persistent: true)
-        lookup_account()
+      def parameterize(opts={})
+        @source = opts.delete(:source) || :production
+
+        error NotImplementedError, "Sandbox Oanda access not implemented- should be easy if you want it." if @source == :sandbox
+        error ArgumentError, "Oanda generator source must be one of [:production, :practice, :sandbox]" unless defaults.present?
+
+        @access_token = opts.delete(:access_token) || ENV[defaults[:access_var] || '']
+
+        error ArgumentError, "No access_token specified. Can also put in env: #{PARAMS.map{|_,d|d[:access_var]}.join(' and/or ')}" if @access_token.blank? && @source != :sandbox
+
+        @api_root     = opts.delete(:api_root)     || defaults[:api_root]
+        @stream_root  = opts.delete(:stream_root)  || defaults[:stream_root]
+
+        @api_conn     = Excon.new(@api_root, headers: {'Authorization' => "Bearer #{@access_token}"}, persistent: true)
+        @account_id   = opts.delete(:account_id)   || lookup_account_id
 
         # Default to all available instruments if none specified
-        @instruments = instruments
-        @instruments = lookup_all_instruments() if @instruments.blank?
+        @instruments  = opts.delete(:instruments)  || lookup_all_instruments
+
+        # Normalize & create an output port for each instrument
         @instruments.map!{|i| Instrument.new(i) }
-
-        # Create an output port for each instrument
         @instruments.each{|i| output("#{i.base}_#{i.counter}".to_sym) } # TODO: specify type (when there is one)
-      end
-
-      def lookup_account()
-        resp        = @api_conn.get!(path: 'v1/accounts', idempotent: true)
-        @accounts   = resp.payload['accounts']
-        error RuntimeError,        "There are no accounts associated with this auth token (#{resp.payload})"  unless @accounts.present? && @accounts[0].present?
-        error NotImplementedError, "Choosing between multiple accounts not implemented"                           if @accounts.size > 1
-        # TODO: implement multiple accounts when it comes up- simply choose primary or override w/ a parameter
-        @account    = @accounts[0]
-        @account_id = @account['accountId']
-        error RuntimeError,        "Could not get valid account id (expecting accountId field): #{@accounts}" unless @account_id && @account_id.to_s =~ /\w/
       end
 
       def lookup_all_instruments()
@@ -85,6 +75,19 @@ module Nodus
         #
         :done
       end
+
+      private
+      def lookup_account_id()
+        resp     = @api_conn.get!(path: 'v1/accounts', idempotent: true)
+        accounts = resp.payload['accounts']
+        error RuntimeError,        "There are no accounts associated with this auth token (#{resp.payload})"  unless accounts.present? && accounts[0].present?
+        error NotImplementedError, "Choosing between multiple accounts not implemented"                           if accounts.size > 1
+        # TODO: implement multiple accounts when it comes up- simply choose primary or override w/ a parameter
+        account_id = accounts[0]['accountId']
+        error RuntimeError, "Could not get valid account id (expecting accountId field): #{accounts}" unless account_id && account_id.to_s =~ /\w/
+        account_id
+      end
+
     end
   end
 end
--- test/test_fx_oanda.rb
+++ test/test_fx_oanda.rb
@@ -4,7 +4,7 @@ include Nodus::FX
 
 describe Nodus::FX::Oanda do
   it 'fails when given an unknown source type' do
-    ->{Oanda.new(:weird_source)}.must_raise ArgumentError
+    ->{Oanda.new(source: :weird_source)}.must_raise ArgumentError
   end
 
   let(:oanda_gen) { Oanda.new }
@@ -17,9 +17,11 @@ describe Nodus::FX::Oanda do
     oanda_gen.instruments.size.must_be :>, 10
   end
 
-  it 'has at least one of the main pairs' do
+  it 'has by default at least one of the main pairs' do
     symbols = oanda_gen.instruments.map{|i| i.base }
     symbols.must_include :eur
     symbols.must_include :usd
   end
+
+
 end
