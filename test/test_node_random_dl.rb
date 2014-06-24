99bd52a23716e455b9062eea985b28780db62365
Joseph Wecker <joseph.wecker@exsig.com>
2014-06-24 15:10:33 -0700

    A quick aggregating node for laplacian random walks

--- test/test_node_random_dl.rb
+++ test/test_node_random_dl.rb
@@ -1,7 +1,9 @@
 require 'helper'
 require 'nodus/nodes/rand_dl'
+require 'nodus/nodes/rand_walk'
 
 DL = Nodus::Nodes::Random::DL
+Walk = Nodus::Nodes::Random::Walk
 
 describe DL do
   it 'loads correctly' do
@@ -11,6 +13,13 @@ describe DL do
 
   it 'outputs a bunch of random junk' do
     d = DL.new
-    100.times{ puts d.y.receive }
+    100.times{ d.y.receive.must_be_kind_of Integer }
+  end
+end
+
+describe Walk do
+  it 'outputs a bunch of stuff' do
+    w = Walk.new
+    1000.times{ puts w.y.receive }
   end
 end
