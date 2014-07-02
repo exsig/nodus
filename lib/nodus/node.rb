098974372dd1b14ad73897a05d3689948bfda90c
Joseph Wecker <joseph.wecker@exsig.com>
2014-07-01 21:18:42 -0700

    Add very basic skeleton and beginning of some interface notes for node and token

---
+++ lib/nodus/node.rb
@@ -0,0 +1,23 @@
+
+
+
+# - [ ] Input-duals
+# - [ ] Output-duals
+# - [ ] static composition
+# - [ ] dynamic composition
+
+# - [ ] simple generators
+
+module Nodus
+  class Node
+
+  end
+
+
+  # - [ ] sequence state startup for tokens
+  # - [ ] automatic (or manual) token creation
+  # - [ ] automatic session creation (for now)
+  class Generator < Node
+
+  end
+end
---
+++ lib/nodus/token.rb
@@ -0,0 +1,15 @@
+
+
+
+# - [ ] modification graph
+# - [ ] timings in graph
+# - [ ] current-data per thread/context
+# - [ ] data appending
+# - [ ] data freezing/locking per thread/context
+#
+
+module Nodus
+  class Token
+
+  end
+end
