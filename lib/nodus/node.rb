21c81eaf824e0a9b2f5c72a8de2893850fd8223c
Joseph Wecker <joseph.wecker@exsig.com>
2014-07-02 19:38:43 -0700

    Start to reimplement new node interface in light of the new architecture. Define ports in basic case.

--- lib/nodus/node.rb
+++ lib/nodus/node.rb
@@ -10,7 +10,14 @@
 
 module Nodus
   class Node
-
+    def self.c_inputs () @c_inputs  ||= [] end
+    def self.c_outputs() @c_outputs ||= [] end
+    def      inputs   () @inputs    ||= self.class.c_inputs.dup  end
+    def      outputs  () @outputs   ||= self.class.c_outputs.dup end
+    def self.input    (name, desc=nil) c_inputs  << OpenStruct.new(name: name, desc: desc) end
+    def self.output   (name, desc=nil) c_outputs << OpenStruct.new(name: name, desc: desc) end
+    def      input    (name, desc=nil) inputs    << OpenStruct.new(name: name, desc: desc) end
+    def      output   (name, desc=nil) outputs   << OpenStruct.new(name: name, desc: desc) end
   end
 
 
---
+++ test/core/test_node.rb
@@ -0,0 +1,70 @@
+require_relative '../helper.rb'
+include Nodus
+
+describe Nodus::Node do
+  after do
+    remove_class :ExampleNode
+  end
+
+  it 'allows class-level ports to be defined' do
+    class ExampleNode < Node
+      input  :x
+      input  :y
+      input  :z, "The ~z~"
+      output :h
+      output :i
+      output :j, "The ~j~"
+    end
+
+    s = ExampleNode.new
+    s.inputs.size.must_equal        3
+    s.outputs.size.must_equal       3
+    s.inputs.first.name.must_equal  :x
+    s.outputs.first.name.must_equal :h
+  end
+
+  it 'merges class and instance-level ports' do
+    class ExampleNode < Node
+      input  :x
+      output :y
+      def initialize(*)
+        input  :m
+        output :n
+        super
+      end
+    end
+
+    s = ExampleNode.new
+
+    s.inputs.size.must_equal  2
+    s.outputs.size.must_equal 2
+
+    s.inputs.map{|i|  i.name }.sort.must_equal [:m, :x]
+    s.outputs.map{|i| i.name }.sort.must_equal [:n, :y]
+  end
+
+  it 'allows duplicate port names' do
+    class ExampleNode < Node
+      input  :a
+      input  :x
+      input  :x
+      output :b
+      output :x
+      output :x
+      def initialize(*)
+        input  :x
+        output :x
+        super
+      end
+    end
+
+    s = ExampleNode.new
+
+    s.inputs.size.must_equal  4
+    s.outputs.size.must_equal 4
+
+    s.inputs.map{|i|  i.name }.sort.must_equal [:a, :x, :x, :x]
+    s.outputs.map{|i| i.name }.sort.must_equal [:b, :x, :x, :x]
+  end
+end
+
