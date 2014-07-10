8c9e0695109f6a577aa4ba894fd7c435d9c9bbf5
Joseph Wecker <joseph.wecker@exsig.com>
2014-07-09 19:42:55 -0700

    Test more traditional class creation via composition

--- test/core/test_node.rb
+++ test/core/test_node.rb
@@ -19,10 +19,10 @@ describe Nodus::Nodes::Node do
     end
   end
 
-  it 'is a node, with correct name' do
+  it 'is a node, with correct title' do
     subject.must_be_a_node
     subject.new.must_be_kind_of Node
-    subject.name.must_equal :subject
+    subject.title.must_equal :subject
   end
 
   it 'has the correct parameters' do
@@ -30,8 +30,45 @@ describe Nodus::Nodes::Node do
     subject.parameters.p3.default.must_equal 100
   end
 
-  it 'ensures that there is a name' do
+  it 'ensures that there is a title' do
     ->{Node.compose()}.must_raise ArgumentError
+    ->{Node[]}.must_raise         ArgumentError
+  end
+
+  it 'children having children having children' do
+    ssubject = subject.compose(:ssubject)
+    ssubject.must_be_a_node
+    ssubject.parameters.keys.sort.must_equal [:p1, :p2, :p3]
+    ssubject.parameters.p3.default.must_equal 100
+
+    sssubject = ssubject.compose(:sssubject){ param :p4 }
+    sssubject.must_be_a_node
+    sssubject.parameters.keys.sort.must_equal [:p1, :p2, :p3, :p4]
+    sssubject.parameters.p3.default.must_equal 100
+  end
+
+  it 'can be done using a more natural class syntax' do
+    class MySuperNode < Node.compose :my_super_node
+      param :p2, :hidden
+    end
+    MySuperNode.must_be_a_node
+    MySuperNode.parameters.p2.hidden?.must_be_true
+    remove_class(:MySuperNode)
+
+    class MyOtherNode < subject.compose :my_other_node
+      param :pzz
+    end
+    MyOtherNode.must_be_a_node
+    MyOtherNode.parameters.pzz.must_be_kind_of Param
+    remove_class(:MyOtherNode)
+
+    class MyThirdNode < Node[:the_third]; end
+    MyThirdNode.must_be_a_node
+    MyThirdNode.parameters.must_be_empty
+    #pp MyThirdNode
+    MyThirdNode.title.must_equal :the_third
+
+    remove_class(:MyThirdNode)
   end
 end
 
@@ -50,6 +87,11 @@ describe Nodus::Nodes::ConcurrentNode do
     conc = ConcurrentNode.compose(:conc, base_1, base_2)
     conc.must_be_a_node
     conc.new.must_be_kind_of Node
+    conc.title.must_equal :conc
+  end
+
+  it 'also requires a title (for now)' do
+    ->{ConcurrentNode.compose(base_1, base_2)}.must_raise ArgumentError
   end
 
   it 'non-conflicting consolidated params act normal' do
