72a5c2b17451e570ded3131af07261d59b1d2a8d
Joseph Wecker <joseph.wecker@gmail.com>
2014-07-09 23:01:10 -0700

    Add comment about disjoint sets of names, and remove cruft at bottom of nodes.rb that isn't needed any more

--- lib/nodus/nodes.rb
+++ lib/nodus/nodes.rb
@@ -20,6 +20,10 @@ module Nodus
     #  inverse input: :output
     #end
 
+    # I think we'll want the following to be completely disjoint:
+    # (common methods) ⊔ (node methods) ⊔ (parameter names) ⊔ (node names) ⊔ (stream-port names)
+    # This way we can do method_missing safely to specify params, nodes, ports, or anything else depending on the context.
+    #
     class Node
       class_attr_inheritable :parameters, PropList.new(Param)
       class_attr_inheritable :title,      nil
@@ -129,58 +133,3 @@ end
 #
 #    ability to rename params & ports as they get composed... (or as something else done while currying etc...)
 #
-
-
-
-
-        # CLASS LEVEL CURRYING
-        # def new_parameterized_class(newname, param_defs={})
-        #   current_parameters = parameters
-        #   klass = Class.new(self) do |mod|
-        #     # Merge/append with new parameters
-        #     param_defs.each{|name, opts| mod.param(name, opts)}
-        #   end
-        #   Object.const_set(newname, klass)
-        # end
-
-
-  # class Node
-  #   attr_reader :name
-
-  #   class << self
-  #     def [](*args) new(*args) end
-
-  #     # Hold stream inputs/outputs defined at the class level
-  #     def c_inputs () @c_inputs  ||= [] end
-  #     def c_outputs() @c_outputs ||= [] end
-
-  #     # Define class-level inputs/outputs
-  #     protected def input    (*names) c_inputs.concat (names.flatten) end
-  #     protected def output   (*names) c_outputs.concat(names.flatten) end
-  #   end
-
-  #   # For defining instance-level inputs/outputs
-  #   protected def input (*names) inputs.concat (names.flat_map{|n|  InputStreamPort.new(self,n)}) end
-  #   protected def output(*names) outputs.concat(names.flat_map{|n| OutputStreamPort.new(self,n)}) end
-
-  #   # Instance inputs/outputs (initialized with a copy of the class-defined ones)
-  #   def inputs () @inputs  ||= FlexArray.new(self.class.c_inputs .map{|n|  InputStreamPort.new(self, n)}) end
-  #   def outputs() @outputs ||= FlexArray.new(self.class.c_outputs.map{|n| OutputStreamPort.new(self, n)}) end
-
-  #   def initialize(name=nil)
-  #     @name = name || self.class.name
-  #   end
-
-  #   def next_input
-  #     error(AmbiguousBinding, inputs) if inputs.size > 1
-  #     inputs[0].next_input
-  #   end
-
-  #   def next_output
-  #     error(AmbiguousBinding, outputs) if outputs.size > 1
-  #     outputs[0].next_output
-  #   end
-
-  #   def listen_to     (other_port) next_input.listen_to      (other_port) end
-  #   def add_subscriber(other_port) next_output.add_subscriber(other_port) end
-  # end
